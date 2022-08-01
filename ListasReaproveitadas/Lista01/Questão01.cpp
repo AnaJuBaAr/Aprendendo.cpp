41428a216c0c1bd4b43c02899b38c339400b7fb0
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-07-31 22:29:30 -0300

    Add files via upload

--- ListasReaproveitadas/Lista01/Questão01.cpp
+++ ListasReaproveitadas/Lista01/Questão01.cpp
@@ -1,1 +1,9 @@
 //Escreva um programa que receba dois inteiros e imprima a soma dos dois valores.
+#include <iostream>;
+int main() {
+	int A, B;
+	std::cin >> A >> B;
+	std::cout << A + B << std::endl;
+	system("PAUSE");
+	return 0;
+}
