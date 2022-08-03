0a7164b496069fa1184b007e316f35e648b94f28
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-03 15:16:17 -0300

    Delete Questão01.cpp

--- ListasReaproveitadas/Lista01/Questão01.cpp
+++
@@ -1,9 +0,0 @@
-//Escreva um programa que receba dois inteiros e imprima a soma dos dois valores.
-#include <iostream>;
-int main() {
-	int A, B;
-	std::cin >> A >> B;
-	std::cout << A + B << std::endl;
-	system("PAUSE");
-	return 0;
-}
