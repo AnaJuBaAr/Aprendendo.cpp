2897aa74d1c91a3c60d1790ee7f28f994ed977e8
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-03 15:18:39 -0300

    Create Questão06.cpp

---
+++ CPP/Lista01/Questão06.cpp
@@ -0,0 +1,10 @@
+/*Reescreva o programa anterior para que solicite do usu�rio a quantidade de metros que deseja
+converter e depois de realizar a convers�o informe ao usu�rio a quantidade de polegadas.*/
+#include <iostream>
+int main() {
+	int A;
+	std::cin >> A;
+	std::cout << A * 30.3700787 << std::endl;
+	system("PAUSE");
+	return 0;
+}
