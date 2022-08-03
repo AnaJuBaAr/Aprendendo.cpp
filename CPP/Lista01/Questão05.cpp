a215755f99496857c7885dc66e25ee22ccf5454e
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-03 15:17:29 -0300

    Create Questão05.cpp

---
+++ CPP/Lista01/Questão05.cpp
@@ -0,0 +1,11 @@
+/*Um profissional precisa de um software que converta valores em metros para valores em
+polegadas. Sabendo que 1 metro equivale a 39,3700787 polegadas, crie um programa que fa�a
+o c�lculo de quantas polegadas equivale a 30 metros.*/
+#include <iostream>
+int main() {
+  double A;
+	A = 30 * 39.3700787;
+	std::cout << A << " polegadas equivalem a 30 metros." << std::endl;
+	system("PAUSE");
+	return 0;
+}
