bd9ec6af72a7708859137079f6fd754eb537dddf
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-03 15:19:18 -0300

    Create Questão07.cpp

---
+++ CPP/Lista01/Questão07.cpp
@@ -0,0 +1,9 @@
+/*Alonso decidiu passear pelas cidades pr�ximas da regi�o onde mora. Para conhecer os locais, ele gastou 2 
+horas percorrendo uma dist�ncia de 120 km. Escreva um programa que calcule e imprima a velocidade que Alonso
+estava em seu passeio.*/
+#include <iostream>;
+int main() {
+	std::cout << 120 / 2 << std::endl;
+	system("PAUSE");
+	return 0;
+}
