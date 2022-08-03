b2f24a328cd67e29e7fd1cdc46ad8d28d45a1778
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-03 15:15:54 -0300

    Create Questão04.cpp

---
+++ CPP/Lista01/Questão04.cpp
@@ -0,0 +1,9 @@
+/*Escreva um programa que receba quatro notas de um aluno e imprima a m�dia das notas.*/
+#include <iostream>;
+int main() {
+	double N1, N2, N3, N4;
+	std::cin >> N1 >> N2 >> N3 >> N4;
+	std::cout << (N1 + N2 + N3 + N4) / 4 << std::endl;
+	system("PAUSE");
+	return 0;
+}
