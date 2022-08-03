c1c47f0c0a3f56fbb84a4eb30cfc3564448b1524
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-03 15:14:08 -0300

    Create Questão02.cpp

---
+++ CPP/Lista01/Questão02.cpp
@@ -0,0 +1,11 @@
+/*Escreva um programa que receba dois n�meros decimais, armazene a soma dos dois em umanova vari�vel e em 
+seguida imprima o resultado.*/
+#include <iostream>;
+int main() {
+	double A, B, R;
+	std::cin >> A >> B;
+	R = A + B;
+	std::cout << R << std::endl;
+	system("PAUSE");
+	return 0;
+}
